08cd21fecbf802d742d13571bef685ea7f6df56d
jafari254 <arumbwajafari@gmail.com>
2024-03-09 23:32:35 +0300

    dart data types

---
+++ data-types.dart
@@ -0,0 +1,29 @@
+void main () {
+  //number is used to print both double and integers
+  num years = 32;
+  num height =5.9;
+ print("your age is $years years and your height is $height feet");
+  //double type is used for numbers with decimal places
+  var double;
+  double= 33.671;
+  print(double);
+  //int is used for whole numbers only
+  int fare = 2500;
+print(fare);
+//string type is used for writing out characters
+String name = "obwondi";
+print(name);
+/*maps are like dictionaries in python. They are a collection
+that represents a set of values as key-value pairs */
+// Declaring a map named ages
+Map<String, int> ages = {'Alice': 25,'Bob': 30,'Charlie': 28,'Diana': 35,'Eric': 22,};
+print(ages);
+/*A list is an ordered collection of elements that can contain duplicate values. Lists
+in Dart are similar to arrays in other programming languages. Lists can contain elements
+of any data type, including other lists.*/
+List<int> numbers = [1, 2, 3, 4, 5];
+print(numbers);
+/*In Dart, a rune represents a Unicode code point. Unicode is a standard for representing
+characters from different writing systems around the world, and each character is assigned
+a unique code point. Runes are used to represent these code points in Dart.*/
+}
